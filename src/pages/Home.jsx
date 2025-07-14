60eab0ec01daee9cbeb6d6b18fa9b1e5bb925413
Tonyreng <tonyreng01@gmail.com>
2025-07-14 00:36:33 +0000

    Creando fetch para crear usuario y hacer get de los contactos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,61 @@
+import { Box } from "@mui/material";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { useEffect } from "react";
 
 export const Home = () => {
+  const { store, dispatch } = useGlobalReducer();
 
-  const {store, dispatch} =useGlobalReducer()
+  const createUser = async () => {
+    try {
+      const response = await fetch(
+        "https://playground.4geeks.com/contact/agendas/tony",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return console.error(error);
+    }
+  };
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-		</div>
-	);
-}; 
+  useEffect(() => {
+    const getContactList = async () => {
+      try {
+        const response = await fetch(
+          "https://playground.4geeks.com/contact/agendas/tony/contacts"
+        );
+        if (response.status === 404) {
+          await createUser();
+          return getContactList();
+        }
+        const data = await response.json();
+        if (data.contacts?.length) {
+          dispatch({
+            type: "set_contact",
+            payload: data.contacts,
+          });
+        }
+      } catch (error) {
+        return console.error(error);
+      }
+    };
+
+    getContactList();
+  }, []);
+
+  return (
+    <div className="text-center mt-5">
+      <Box></Box>
+      <h1>Hello Rigo!!</h1>
+      <p>
+        <img src={rigoImageUrl} />
+      </p>
+    </div>
+  );
+};
