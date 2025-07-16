a4051d380ac6e4bc726a459e6a062b5f6a9b3d56
Tonyreng <tonyreng01@gmail.com>
2025-07-16 03:45:33 +0000

    Se refactorizo boton en el navbar para abrir modal para crear contacto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,37 @@
 import { Link } from "react-router-dom";
+import { Modal, Box, Typography, Button } from "@mui/material";
+import { useState } from "react";
+import { NewContactModal } from "../components/NewContactModal";
 
 export const Navbar = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+        </Link>
+        <div className="ml-auto">
+          <Button
+            variant="contained"
+            onClick={() => {
+              handleOpenModal();
+            }}
+          >
+            Add new contact
+          </Button>
+        </div>
+      </div>
+      <NewContactModal open={isModalOpen} onClose={handleCloseModal} />
+    </nav>
+  );
 };
