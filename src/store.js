f61a7b53b87526b4afa66d61d788fc28af4cc924
Tonyreng <tonyreng01@gmail.com>
2025-07-17 23:40:27 +0000

    Se agrego el dispatch para agregar automaticamente el nuevo contacto al store

--- src/store.js
+++ src/store.js
@@ -12,6 +12,11 @@ export default function storeReducer(store, action = {}) {
         ...store,
         contacts: action.payload,
       };
+    case "add_contact":
+      return {
+        ...store,
+        contacts: [...store.contacts, action.payload],
+      };
     default:
       throw Error("Unknown action.");
   }
